e5f32a604467f81b850c9f3ee0e1b0732fed0d8f
petercheng <petercheng7788@gmail.com>
2021-10-18 00:25:29 +0800

    chord: test webhook

--- server.ts
+++ server.ts
@@ -9,6 +9,12 @@ const app = express();
 const port = process.env.PORT ?? 5000;
 app.use(express.json());
 
+// bot.launch();
+bot.telegram.setWebhook(
+  `https://my-jarvis-backend.herokuapp.com/bot${process.env.BOT_TOKEN}`
+);
+app.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
+
 app.get("/hello", (req: Request, res: Response) => {
   console.log(process.env.MY_TELEGRAM_CHAT_ID, process.env.BOT_TOKEN);
   res.send("hello");
@@ -33,12 +39,6 @@ app.post("/telegramMsg", (req: Request, res: Response) => {
   }
   res.status(400).send("Server Error !!!");
 });
-
-// bot.launch();
-bot.telegram.setWebhook(
-  `https://my-jarvis-backend.herokuapp.com/bot${process.env.BOT_TOKEN}`
-);
-app.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
 
 app.listen(port, () => {
   console.log("Server Start at port", port);
