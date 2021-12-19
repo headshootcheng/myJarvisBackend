16fe9e0ff1f0208591fb8809ede28aa9b3b43ec5
petercheng <petercheng7788@gmail.com>
2021-12-20 00:30:11 +0800

    feat: push notification api

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import express, { Request, Response } from "express";
 import { scheduleJob } from "node-schedule";
 import { Telegraf, Context } from "telegraf";
+import axios from "axios";
 require("dotenv").config();
 
 const bot = new Telegraf<Context>(process.env.BOT_TOKEN ?? "");
@@ -21,7 +22,7 @@ bot.telegram.sendMessage(
 );
 
 app.get("/hello", (req: Request, res: Response) => {
-  res.send("hello123");
+  res.send("hello");
 });
 
 app.post("/telegramMsg", (req: Request, res: Response) => {
@@ -32,7 +33,7 @@ app.post("/telegramMsg", (req: Request, res: Response) => {
       console.log(`It's times to do event ${alertText}`);
       bot.telegram.sendMessage(
         process.env.MY_TELEGRAM_CHAT_ID ?? 2048189240,
-        `It's times to do event ${alertText}`
+        `喂！夠鐘做 ${alertText}`
       );
     });
     res.send(
@@ -43,6 +44,36 @@ app.post("/telegramMsg", (req: Request, res: Response) => {
     return;
   }
   res.status(400).send("Server Error !!!");
+});
+
+app.post("/well-saying", async (req: Request, res: Response) => {
+  try {
+    const { data: dictData } = await axios.get(
+      `${process.env.TIAN_API}/dictum/index?key=${process.env.TIAN_KEY}&num=1`
+    );
+    const message = {
+      to: process.env.APP_EXPO_TOKEN,
+      sound: "default",
+      title: "溫馨提示",
+      body: `${dictData.newslist[0].content} --- ${dictData.newslist[0].mrname}`,
+      data: { someData: "goes here" },
+    };
+    const expoRes = await axios.post(
+      "https://exp.host/--/api/v2/push/send",
+      message,
+      {
+        headers: {
+          Accept: "application/json",
+          "Accept-encoding": "gzip, deflate",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.send("Post Success!!!");
+  } catch (err) {
+    console.log("post Expo notification failed: ", err);
+    res.status(400).send("post Expo notification failed");
+  }
 });
 
 app.listen(port, () => {
